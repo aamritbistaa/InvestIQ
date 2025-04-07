ae62ad0e3946ae8520916e4c45d145504fdec45e
aamritbistaa <aamritbistaa@gmail.com>
2025-04-07 21:45:49 +0545

    added portfolio get section

---
+++ api/Controller/PortfolioController.cs
@@ -0,0 +1,35 @@
+using api.Extensions;
+using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PortfolioController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IStockRepository _stockRepository;
+        private readonly IPortfolioRepository _portfolioRepository;
+        public PortfolioController(UserManager<AppUser> userManager, IStockRepository stockRepository, IPortfolioRepository portfolioRepository)
+        {
+            _userManager = userManager;
+            _stockRepository = stockRepository;
+            _portfolioRepository = portfolioRepository;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetUserPortfolio()
+        {
+            var username = User.GetUserName();
+            var appUser = await _userManager.FindByNameAsync(username);
+            var userPortfolio = _portfolioRepository.GetUserPortfolio(appUser);
+            return Ok(userPortfolio);
+        }
+    }
+}
---
+++ api/Repository/PortfolioRepository.cs
@@ -0,0 +1,29 @@
+using System;
+using api.Data;
+using api.Interfaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository;
+
+public class PortfolioRepository : IPortfolioRepository
+{
+    private readonly ApplicationDbContext _context;
+    public PortfolioRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public async Task<List<Stock>> GetUserPortfolio(AppUser user)
+    {
+        return await _context.Portfolios.Where(x => x.AppUserId == user.Id).Select(x => new Stock
+        {
+            Id = x.StockId,
+            Symbol = x.Stock.Symbol,
+            CompanyName = x.Stock.CompanyName,
+            Purchase = x.Stock.Purchase,
+            LastDiv = x.Stock.LastDiv,
+            Industry = x.Stock.Industry,
+            MarketCap = x.Stock.MarketCap,
+        }).ToListAsync();
+    }
+}
