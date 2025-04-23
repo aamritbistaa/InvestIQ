16102377973edad56034085dc8ca346712dfec4c
aamritbistaa <aamritbistaa@gmail.com>
2025-04-23 22:54:55 +0545

    removed frontend to start from first

--- frontend/src/Components/CompanyProfile/CompanyProfile.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const CompanyProfile = (props: Props) => {
-  return <div>CompanyProfile</div>;
-};
-
-export default CompanyProfile;
--- frontend/src/Components/IncomeStatement/IncomeStatement.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const IncomeStatement = (props: Props) => {
-  return <div>IncomeStatement</div>;
-};
-
-export default IncomeStatement;
